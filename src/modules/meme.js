5ff0b35ef37913ecb64ee7a49f4dacb1e5eea84c
xVemu <kamilox26@gmail.com>
2020-06-13 17:18:01 +0200

    * add meme

---
+++ src/modules/meme.js
@@ -0,0 +1,21 @@
+`use strict`;
+
+const axios = require(`axios`);
+
+module.exports = {
+    name: `meme`,
+    description: `Sends random meme.`,
+    args: 0,
+    guildOnly: false,
+    aliases: [],
+    async execute(msg) {
+        try {
+            const { data: { nsfw, url, title }} = await axios.get(`https://meme-api.herokuapp.com/gimme`);
+            if (nsfw && !msg.channel.nsfw) return this.execute(msg);
+            msg.channel.send(title, { files: [url] });
+        } catch (e) {
+            msg.reply(`Error has occured!`);
+            console.error(e);
+        }
+    }
+};
