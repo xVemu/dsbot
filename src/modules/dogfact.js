d4e6000d79a0474d2dc93b3aee6c7993230b8577
MaestroDagan <ditisdagan@hotmail.com>
2022-10-04 21:59:37 +0200

    Requested Changes

--- src/modules/dogfact.js
+++ src/modules/dogfact.js
@@ -8,6 +8,6 @@ module.exports = {
     description: 'Sends random fact about dogs.',
     async execute(msg) {
         const {data: {facts}} = await axios.get('https://dog-api.kinduff.com/api/facts')
-        await msg.reply({embeds: [new EmbedBuilder().setTitle("Random dog fact!").setDescription(facts[0]).setColor('Random').setTimestamp()]})
+        await msg.reply({embeds: [new EmbedBuilder({title: 'Random dog fact!', description: facts[0], color: 'Random', timestamp: Date.now(), footer: {text: 'Mover Bot'}}).setTitle('Random dog fact!').setDescription(facts[0]).setColor('Random').setTimestamp()]})
     },
 }
