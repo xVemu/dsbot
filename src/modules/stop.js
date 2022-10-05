4bb57acf9a33182caca55b1e73828510111e8fbb
Kamil Kras <38427679+xVemu@users.noreply.github.com>
2022-10-05 20:17:49 +0200

    Add option to not list module in command list

--- src/modules/stop.js
+++ src/modules/stop.js
@@ -1,19 +1,10 @@
-'use strict'
-
-const {CommandInteraction} = require('discord.js')
-
-/*TODO*/
 module.exports = {
-    name: 'stop',
-    description: 'Stops moving!',
-    dmPermission: false,
-    async execute(msg) {
-        require('./move').shouldMoving = false
-        if (msg instanceof CommandInteraction)
-            await msg.reply('Stopped')
-    },
-    async buttonClick(interaction) {
-        require('./move').shouldMoving = false
-        await interaction.message.delete()
-    },
+  name: 'stop',
+  description: 'Stops moving!',
+  isCommand: false,
+  async execute(msg) {
+    // eslint-disable-next-line global-require
+    require('./move').shouldMoving = false
+    await msg.message.delete()
+  },
 }
