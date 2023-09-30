68f27c914e5d50fbbfcd6839c6316b01b18f8694
MaestroDagan <ditisdagan@hotmail.com>
2023-09-30 18:38:41 +0200

    feat: :sparkles: /uselessfact
    #25

---
+++ src/modules/uselessfact.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+import { EmbedBuilder } from 'discord.js';
+
+export default {
+  name: 'uselessfact',
+  description: 'Sends random useless fact.',
+  async execute(msg) {
+    const {
+      data: { text },
+    } = await axios.get('https://uselessfacts.jsph.pl/api/v2/facts/today');
+    console.log(`🚀 ~ file: uselessfact.js:10 ~ execute ~ text:`, text);
+
+    await msg.reply({
+      embeds: [
+        new EmbedBuilder({
+          title: 'Random useless fact!',
+          description: text,
+          color: 0x10b5bf,
+          timestamp: Date.now(),
+          footer: { text: 'Mover Bot' },
+        }),
+      ],
+    });
+  },
+};
