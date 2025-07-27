bc2daa263ec7a008ef2c64a2b4d684117d6fbefa
Kamil Kras <38427679+xVemu@users.noreply.github.com>
2025-07-27 19:34:56 +0200

    Update api in lol

--- src/modules/lol.js
+++ src/modules/lol.js
@@ -1,8 +1,6 @@
 import { ApplicationCommandOptionType, EmbedBuilder } from 'discord.js'
-import Axios from 'axios'
-import config from '../config.cjs'
-
-const axios = Axios.create({ headers: { 'X-Riot-Token': config.lolKey } })
+import config from '../../config.json'
+import { fetchJson } from '../utils.js'
 
 const regions = {
   BR: 'br1',
@@ -16,6 +14,8 @@ const regions = {
   OC: 'oc1',
   RU: 'ru',
   TR: 'tr1',
+  TW: 'TW2',
+  VN: 'VN2'
 }
 
 const queueTypes = {
@@ -24,6 +24,8 @@ const queueTypes = {
   RANKED_TFT: 'TFT: ',
 }
 
+const latestPatch = await fetchJson('https://ddragon.leagueoflegends.com/api/versions.json').then((res) => res[0])
+
 export default {
   name: 'lol',
   description: 'Sends info about player in League Of Legends.',
@@ -31,7 +33,7 @@ export default {
     {
       type: ApplicationCommandOptionType.String,
       name: 'nick',
-      description: 'player\'s nick',
+      description: 'player\'s nick with tag',
       required: true,
     },
     {
@@ -46,40 +48,45 @@ export default {
     },
   ],
   async execute(msg, [nameArg, regionArg]) {
-    try {
       await msg.deferReply()
 
+      const [nick, tag] = nameArg.value.split('#')
       const region = regions[regionArg?.value.toUpperCase()] ?? regions.EUNE
       const baseURL = `https://${region}.api.riotgames.com/`
-      const { data: { id, name, summonerLevel, profileIconId } } = await axios.get(`${baseURL}lol/summoner/v4/summoners/by-name/${nameArg.value}`)
-      const { data: rankList } = await axios.get(`${baseURL}lol/league/v4/entries/by-summoner/${id}`)
+
+      const { puuid } = await fetchJson(
+        `https://europe.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${nick}/${tag}?api_key=${config.lolKey}`)
+    const { summonerLevel, profileIconId } = await fetchJson(
+        `${baseURL}lol/summoner/v4/summoners/by-puuid/${puuid}?api_key=${config.lolKey}`)
+
+      if(!summonerLevel) return msg.editReply('Summoner not found!')
+
+      const rankList = await fetchJson(`${baseURL}lol/league/v4/entries/by-puuid/${puuid}?api_key=${config.lolKey}`)
 
       const embedded = new EmbedBuilder({
-        title: name,
+        title: nameArg.value,
         color: 0x10B5BF,
         author: { name: 'League Of Legends Stats' },
-        thumbnail: { url: `https://ddragon.leagueoflegends.com/cdn/12.18.1/img/profileicon/${profileIconId}.png` },
+        thumbnail: { url: `https://ddragon.leagueoflegends.com/cdn/${latestPatch}/img/profileicon/${profileIconId}.png` },
         footer: { text: 'Mover Bot' },
-        fields: [{
-          name: 'Level: ',
-          value: summonerLevel.toString(),
-        }, ...rankList.map(({
-          tier,
-          rank,
-          leaguePoints,
-          wins,
-          losses,
-          queueType,
-        }) => ({
-          name: queueTypes[queueType],
-          value: `${tier} ${rank} ${leaguePoints}LP(${wins}W/${losses}P)`,
-        }))],
+        fields: [
+          {
+            name: 'Level: ',
+            value: summonerLevel.toString(),
+          }, ...rankList.map(({
+            tier,
+            rank,
+            leaguePoints,
+            wins,
+            losses,
+            queueType,
+          }) => ({
+            name: queueTypes[queueType],
+            value: `${tier} ${rank} ${leaguePoints}LP - ${Math.round(wins/(losses+wins)*100)}% WR (${wins}W/${losses}L)`,
+          })),
+        ],
       })
 
       await msg.editReply({ embeds: [embedded] })
-    } catch (e) {
-      if (e.response.status === 404) return msg.editReply('Summoner not found!')
-      throw e
-    }
   },
 }
